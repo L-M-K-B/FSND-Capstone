caea20d50274a6eabcabcb9d06a8103a3b13b570
L-M-K-B <Laura.M.K.Benz@gmail.com>
2020-05-09 18:52:38 +0200

    feat: add end points

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def create_dict(query_res):
 def create_app():
     app = Flask(__name__)
     setup_db(app)
-    app = Flask(__name__)
     CORS(app)
 
     @app.after_request
@@ -42,7 +41,7 @@ def create_app():
     def welcome():
         return 'This is the Capstone start page'
 
-    # -- # requests for movie table # -- #
+    # -- # requests movie table # -- #
     @app.route('/movies', methods=['GET'])
     def get_movies():
         movies = Movie.query.all()
@@ -57,7 +56,7 @@ def create_app():
             'movies': movies_dict
         }), 200
 
-    # -- # requests for actresses table # -- #
+    # -- # requests actresses table # -- #
     @app.route('/actresses', methods=['GET'])
     def get_actresses():
         actresses = Actress.query.all()
@@ -71,6 +70,125 @@ def create_app():
             'success': True,
             'actresses': actresses_list
         }), 200
+
+    # -- # posts a new actress # -- #
+    @app.route('/actress', methods=['POST'])
+    def create_new_actress():
+        actress = request.get_json()
+
+        try:
+            new_actress = Actress(**actress)
+            new_actress.insert()
+
+            return jsonify({
+                'success': True,
+                'created': new_actress.id
+            })
+        except SQLAlchemyError:
+            abort(405)
+
+    # -- # deletes an actress # -- #
+    @app.route('/actress/<int:actress_id>', methods=['DELETE'])
+    def delete_actress(actress_id):
+        try:
+            actress = Actress.query.filter(Actress.id == actress_id).one_or_none()
+
+            if not actress:
+                abort(422)
+
+            actress.delete()
+
+            return jsonify({
+                'success': True,
+                'deleted': actress_id,
+            })
+        except SQLAlchemyError:
+            abort(422)
+
+    # -- # modifies an actress # -- #
+    @app.route('/actress/<int:actress_id>', methods=['PATCH'])
+    def update_actress(actress_id):
+        actress = Actress.query.filter(Actress.id == actress_id).one_or_none()
+
+        if not actress:
+            abort(404)
+
+        data = request.get_json()
+
+        if not data:
+            abort(404)
+
+        try:
+            actress.name = data['name']
+        except KeyError:
+            pass
+
+        try:
+            actress.birth_date = data['birth_date']
+        except KeyError:
+            pass
+
+        try:
+            actress.gender = data['gender']
+        except KeyError:
+            pass
+
+        try:
+            actress.movies = data['movies']
+        except KeyError:
+            pass
+
+        try:
+            actress.update()
+
+        except SQLAlchemyError:
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'actress': [actress.format()]
+        }), 200
+
+    # -- # Error Handlers # -- #
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(405)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": "method not allowed"
+        }), 405
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "internal server error"
+        }), 500
 
     return app
 
