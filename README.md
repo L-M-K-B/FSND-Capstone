2dea3f79338035e75275d304655c3a3aa4436f94
L-M-K-B <Laura.M.K.Benz@gmail.com>
2020-05-21 15:36:46 +0200

    feat: enable all test and add endpoints to readme documentation

--- README.md
+++ README.md
@@ -1,1 +1,136 @@
 # FSND-Capstone
+## Introduction
+This is the final project of my fullstack developer nano degree. I used the following technologies:
+ 
+    - Coding in Python 3
+    - Relational database architecture (PostgreSQL)
+    - Modeling data objects with SQLAlchemy
+    - Internet protocols and communication
+    - Developing a Flask API
+    - Testing Flask applications
+    - Authentication and access management with Auth0 and Flask
+    - Role-Based Access Control (RBAC)
+    - Deploying the application with Heroku
+
+## Getting started
+Please install the following dependencies:
+### Python 3
+Follow the instructions in the [python docs](https://docs.python.org/3/using/unix.html#getting-and-installing-the-latest-version-of-python)
+### Other dependencies
+In this folder you can find a `requirements.txt` which you can run with the following command:
+```
+pip3 install -r requirements.txt
+```
+### Database setup `(????????????????????)`
+## Testing
+To run the tests, run
+```
+dropdb capstone-test
+createdb capstone-test
+python test_app.py
+```
+# API Endpoints
+## GET: /movies
+- fetches a dictionary of movies:
+    - keys: ids of movies
+    - values: string, name of movie
+- request arguments: None
+- returns a json file:
+```
+{
+    'success': true,
+    'movies': 
+        {
+            1 : 'Hidden Figures'
+        }
+}
+```
+## GET: /actresses
+- fetches a list of objects containing all actresses
+- each actress entry is formatted as a dictionary containing the following keys: 
+    - birth_date (datetime)
+    - gender (str)
+    - id (int)
+    - movies (list of movie ids)
+    - name (str)
+- request arguments: None
+- returns a json file:
+```
+{
+    'success': true,
+    'actresses': 
+        [
+            {
+                'birth_date': 'Fri, 11 Sep 1970 00:00:00 GMT',
+                'gender': 'female',
+                'id': 1,
+                'movies': [1],
+                'name': 'Taraji P Henson'
+            }
+        ]
+}
+```
+## POST: /actress
+- creates a new actress entry
+- request arguments: 
+    - birth_date (DateTime)
+    - gender (str)
+    - id (int)
+    - movies (list of movie ids)
+    - name (str)
+- returns a json file:
+```
+{
+    'success': true,
+    'created': <actress_id (int)>
+}
+```
+## POST: /movie
+- creates a new movie entry
+- request arguments: 
+    - id (int)
+    - title (str)
+    - release_date (DateTime)
+    - country (str)
+- returns a json file:
+```
+{
+    'success': true,
+    'created': <movie_id (int)>
+}
+```
+## DELETE: /actress/<actress_id>
+- deletes a single actress
+- request arguments: 
+    - id of actress
+- returns a json file:
+```
+{
+    'success': true,
+    'deleted': <actress_id (int)>
+}
+```
+## PATCH: /actress/<actress_id>
+- modifies an actress entry
+- request arguments (changes are not necessarily applied to all arguments): 
+    - birth_date (DateTime)
+    - gender (str)
+    - id (int)
+    - movies (list of movie ids)
+    - name (str)
+- returns a json file:
+```
+{
+    'success': true,
+    'actress': 
+        [
+            {
+                'birth_date': 'Fri, 11 Sep 1970 00:00:00 GMT',
+                'gender': 'female',
+                'id': 1,
+                'movies': [1],
+                'name': 'Taraji P Henson'
+            }
+        ]
+}
+```
--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ from models import setup_db
 class TestCase(unittest.TestCase):
 
     def setUp(self):
-        self.token_assistant = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNhQ1YtZ01rd2RFMnZhSFQ5MjdMcyJ9.eyJpc3MiOiJodHRwczovL2ZzbmQtbG1rYi1jYXBzdG9uZS5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWViODIzZGFkMGZhMWUwYmZlZWYxMjU3IiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODkxMjczNDUsImV4cCI6MTU4OTEzNDU0NSwiYXpwIjoid2R3NkRjcTRrZnNZVmt1djM0YU1OMnlDa1pKYTdTU0ciLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbInJlYWQ6YWN0cmVzc2VzIiwicmVhZDptb3ZpZXMiXX0.BhqNVFg4CkBxztfiApCHimrnvF6YlzS3Yp3TnIfnFz7Ug1h-vIW4jTsRJb4eUPr_X4M8rDsVVMdr0_4Qt5_1D0MWWyh7tgZAXQBBE4-muhWe_yHSgIKJIA2sed5cxgiVvjpoU59ByWvawdkKRXERRURgM75DLpnFoidvz6RGxYhvxFlcXbkvj9gVnppRAUVo476f8qD4r20p6ak_lBF69_OshlP5EgrMKFRL6_03pjieIcQzDUFS1KD5OEBjLF0WlT93TTCdRsAHFMxfosvvgNQkTPby-GAD-zO0Z1k-hgaAxmyDhRQ21S2DmYnOeVzPP3QxdokppDtl7noL6FZngw'
+        self.token_assistant = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNhQ1YtZ01rd2RFMnZhSFQ5MjdMcyJ9.eyJpc3MiOiJodHRwczovL2ZzbmQtbG1rYi1jYXBzdG9uZS5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWViODIzZGFkMGZhMWUwYmZlZWYxMjU3IiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODkzNTAzNzMsImV4cCI6MTU4OTM1NzU3MywiYXpwIjoid2R3NkRjcTRrZnNZVmt1djM0YU1OMnlDa1pKYTdTU0ciLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbInJlYWQ6YWN0cmVzc2VzIiwicmVhZDptb3ZpZXMiXX0.MA2dZhZ82Jd1wwqPN_UagnefV1_kGj7GOQ06SopwcZpvAlV2gBTVLcKdjbUt5QhOsDY5v2X1yPLnL-RiIKo3BWQMnAMnzCdlK2B0k2kVtZXq_Ref6yD7o-FSW3vQ_u_mWMEiwTk80HRqULZwHymKxqx1OmFpV9mtcD7TYaJQS9AsOELgRtYEoKqpocFeSYnueR9qF3cVOaJbP_tzU-nxY5fy_0dKX6dFZxAQix6Al9ZvsRtXuuO2s7_DyOwjaOdoT7V8jkf9_U9KuED46joK3_j9ip6SYsUXlq0JQg7tMP1BYhsfbLAg5CpFTo5JjNRwgJnT9PQ_iY6Y2rkCtsZxbg'
         self.token_producer = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNhQ1YtZ01rd2RFMnZhSFQ5MjdMcyJ9.eyJpc3MiOiJodHRwczovL2ZzbmQtbG1rYi1jYXBzdG9uZS5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWViODI0YTdkMGZhMWUwYmZlZWYxMzBmIiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1ODkxMjc0NDIsImV4cCI6MTU4OTEzNDY0MiwiYXpwIjoid2R3NkRjcTRrZnNZVmt1djM0YU1OMnlDa1pKYTdTU0ciLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImFkZDphY3RyZXNzZXMiLCJhZGQ6bW92aWVzIiwiZGVsZXRlOmFjdHJlc3NlcyIsImRlbGV0ZTptb3ZpZSIsIm1vZGlmeTphY3RyZXNzZXMiLCJtb2RpZnk6bW92aWUiLCJyZWFkOmFjdHJlc3NlcyIsInJlYWQ6bW92aWVzIl19.U8j9cjOiD449Zzmuuj62h6WdYqced7DTx7A8xz5mnJyfdv0McSJkCcYBK6AOb7SqlsoOjjBlRwVKcvhJcbXUD-CxbDx_LKQ2aysve3aQQBaLyNrHVaByiiGHsTVplvxjS0aoazPO-8vzJzQXdp4zUSDxkoqnoMeCXAhDGbpbkQ1Y6Y9JcGUCUDb0LfpSzGqkXnGd2gPh3onRbrUrVnZdk9ujt9PR_tQgxa5DJiumJxf6FWlACYDI666AmAg1LJxcITkjjftDc8Qk42Fx_szj7WF-pf5iYGiK2GCEj9x1waSf6F1pf8eoKAs_izxx1F2LwdvVleZTrxnvAATo81dYFA'
         self.app = create_app()
         self.client = self.app.test_client
@@ -41,93 +41,91 @@ class TestCase(unittest.TestCase):
         pass
 
     # -- # '/movies', methods=['GET'] # -- #
-    # def test_get_movies_without_permission(self):
-    #     res = self.client().get('/movies')
-    #     data = json.loads(res.data)
-    #
-    #     self.assertEqual(res.status_code, 401)
-    #     self.assertEqual(data['success'], False)
+    def test_get_movies_without_permission(self):
+        res = self.client().get('/movies')
+        data = json.loads(res.data)
 
-    # def test_get_movies_assistant(self):
-    #     res = self.client().get('/movies', headers={
-    #         "Authorization": 'bearer '+self.token_assistant})
-    #     data = json.loads(res.data)
-    #
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+
+    def test_get_movies_assistant(self):
+        res = self.client().get('/movies', headers={
+            "Authorization": 'bearer '+self.token_assistant})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
 
     # -- # '/actresses', methods=['GET'] # -- #
-    # def test_get_actresses_producer(self):
-    #     res = self.client().get('/actresses', headers={
-    #         "Authorization": 'bearer '+self.token_producer})
-    #     data = json.loads(res.data)
-    #
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
+    def test_get_actresses_producer(self):
+        res = self.client().get('/actresses', headers={
+            "Authorization": 'bearer '+self.token_producer})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
 
     # -- # '/actress', methods=['POST'] # -- #
-    # def test_create_new_actress(self):
-    #     res = self.client().post('/actress', headers={
-    #         "Authorization": 'bearer '+self.token_producer}, json=self.new_actress)
-    #     data = json.loads(res.data)
-    #
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
+    def test_create_new_actress(self):
+        res = self.client().post('/actress', headers={
+            "Authorization": 'bearer '+self.token_producer}, json=self.new_actress)
+        data = json.loads(res.data)
 
-    #TODO
-    # def test_401_unauthorized_create_new_actress(self):
-    #     res = self.client().post('/actress', headers={
-    #         "Authorization": 'bearer '+self.token_assistant}, json=self.new_actress)
-    #
-    #     self.assertEqual(res.status_code, 401)
-        # self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
 
-    # def test_405_creation_not_allowed(self):
-    #     res = self.client().post('/actress/42', headers={"Authorization": 'bearer '+self.token_producer}
-    #                              , json=self.new_actress)
-    #     data = json.loads(res.data)
-    #
-    #     self.assertEqual(res.status_code, 405)
-    #     self.assertEqual(data['success'], False)
-    #     self.assertEqual(data['message'], 'method not allowed')
+    def test_401_unauthorized_create_new_actress(self):
+        res = self.client().post('/actress', headers={
+            "Authorization": 'bearer '+self.token_assistant}, json=self.new_actress)
+
+        self.assertEqual(res.status_code, 401)
+
+    def test_405_creation_not_allowed(self):
+        res = self.client().post('/actress/42', headers={"Authorization": 'bearer '+self.token_producer}
+                                 , json=self.new_actress)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'method not allowed')
 
     # -- # '/actress/<int:actress_id>', methods=['DELETE'] # -- #
-    # def test_delete_actress(self):
-    #     new_id = self.client().post('/actress', headers={"Authorization": 'bearer '+self.token_producer}
-    #                                 , json=self.new_actress).json['created']
-    #     res = self.client().delete(f'/actress/{new_id}', headers={"Authorization": 'bearer '+self.token_producer})
-    #     data = json.loads(res.data)
-    #
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
+    def test_delete_actress(self):
+        new_id = self.client().post('/actress', headers={"Authorization": 'bearer '+self.token_producer}
+                                    , json=self.new_actress).json['created']
+        res = self.client().delete(f'/actress/{new_id}', headers={"Authorization": 'bearer '+self.token_producer})
+        data = json.loads(res.data)
 
-    # def test_422_actress_does_not_exist(self):
-    #     res = self.client().delete('/actress/42', headers={"Authorization": 'bearer '+self.token_producer})
-    #     data = json.loads(res.data)
-    #
-    #     self.assertEqual(res.status_code, 422)
-    #     self.assertEqual(data['success'], False)
-    #     self.assertEqual(data['message'], 'unprocessable')
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_422_actress_does_not_exist(self):
+        res = self.client().delete('/actress/42', headers={"Authorization": 'bearer '+self.token_producer})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
 
     # -- # '/actress/<int:actress_id>', methods=['PATCH'] # -- #
-    # def test_patch_actress(self):
-    #     new_id = self.client().post('/actress', headers={"Authorization": 'bearer '+self.token_producer},
-    #                                 json=self.new_actress).json['created']
-    #     res = self.client().patch(f'/actress/{new_id}', headers={"Authorization": 'bearer '+self.token_producer},
-    #                               json=self.modify_actress)
-    #     data = json.loads(res.data)
-    #
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
+    def test_patch_actress(self):
+        new_id = self.client().post('/actress', headers={"Authorization": 'bearer '+self.token_producer},
+                                    json=self.new_actress).json['created']
+        res = self.client().patch(f'/actress/{new_id}', headers={"Authorization": 'bearer '+self.token_producer},
+                                  json=self.modify_actress)
+        data = json.loads(res.data)
 
-    # def test_404_creation_not_allowed(self):
-    #     res = self.client().patch(f'/actress/42', headers={"Authorization": 'bearer '+self.token_producer},
-    #                               json=self.modify_actress)
-    #     data = json.loads(res.data)
-    #
-    #     self.assertEqual(res.status_code, 404)
-    #     self.assertEqual(data['success'], False)
-    #     self.assertEqual(data['message'], 'resource not found')
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_404_creation_not_allowed(self):
+        res = self.client().patch(f'/actress/42', headers={"Authorization": 'bearer '+self.token_producer},
+                                  json=self.modify_actress)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
 
 
 if __name__ == "__main__":
