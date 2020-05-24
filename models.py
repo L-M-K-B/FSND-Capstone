9a386c5c038a9a540d8af37697fadcd1bdd961f3
L-M-K-B <Laura.M.K.Benz@gmail.com>
2020-05-24 21:40:26 +0200

    refactor: test

--- models.py
+++ models.py
@@ -13,12 +13,12 @@ def setup_db(app, database_path=DATABASE_URL):
     db.app = app
     db.init_app(app)
     db.create_all()
-    new_movie = Movie(title="Hidden Figures", release_date="2016-12-25T00:00:00.000Z", country="USA")
-    new_movie.insert()
-
-    new_actress = Actress(name='Taraji P Henson', birth_date='1970-09-11T00:00:00.000Z', gender='female',
-                          movies=[new_movie.id])
-    new_actress.insert()
+    # new_movie = Movie(title="Hidden Figures", release_date="2016-12-25T00:00:00.000Z", country="USA")
+    # new_movie.insert()
+    #
+    # new_actress = Actress(name='Taraji P Henson', birth_date='1970-09-11T00:00:00.000Z', gender='female',
+    #                       movies=[new_movie.id])
+    # new_actress.insert()
 
 
 def db_drop_and_create_all():
